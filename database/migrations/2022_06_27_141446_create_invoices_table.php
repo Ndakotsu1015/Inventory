796b90019816e8d6255b5f82f4a68082c70c761d
Mohammed Aliyu <maliyu1015@gmail.com>
2022-06-27 17:07:42 +0100

    my first commit

---
+++ database/migrations/2022_06_27_141446_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('invoiceNo');
+            $table->foreignId('client_id')->constrained();
+            $table->date('date_invoice');
+            $table->date('due_date');
+            $table->string('created_by');
+            $table->foreignId('staff_id');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
